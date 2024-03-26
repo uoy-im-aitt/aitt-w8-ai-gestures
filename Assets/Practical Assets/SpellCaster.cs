c44b239ddd470c4d7e424ad2c59a10e4ea4f476d
Jonathan Hook <jonathan.hook@york.ac.uk>
2024-03-26 12:58:05 +0000

    Spell casting working visually

---
+++ Assets/Practical Assets/SpellCaster.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpellCaster : MonoBehaviour
+{
+    public int spellToCast = 0;
+    
+    public Color[] spells;
+    public float spellMovementSpeed = 20.0f;
+    public GameObject spawnPoint;
+    public GameObject target;
+    public GameObject spellPrefab;
+
+    private int lastSpell = 0;
+
+    void Update()
+    {
+        for(int i = 0; i < spells.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                spellToCast = i+1;
+            }
+        }
+     
+        if(spellToCast != lastSpell)
+        {
+            lastSpell = spellToCast;
+            CastSpell();
+        }
+    }
+
+    void CastSpell()
+    {
+        GameObject spellObj = Instantiate(spellPrefab);
+        spellObj.transform.position = spawnPoint.transform.position;
+
+        SpellController controller = spellObj.GetComponent<SpellController>();
+        controller.Init(target, spellMovementSpeed, spells[spellToCast-1]);
+    }
+}
---
+++ Assets/Practical Assets/SpellController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellController : MonoBehaviour
+{
+    private const float DestroyDistance = 0.01f;
+
+    private Vector3 target;
+    private float speed;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target) < DestroyDistance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+    }
+
+    public void Init(GameObject target, float speed, Color color)
+    {
+        this.target = target.transform.position;
+        this.speed = speed;
+
+        Light spellLight = GetComponentInChildren<Light>();
+        spellLight.color = color;
+
+        ParticleSystem.MainModule spellParticles = GetComponentInChildren<ParticleSystem>().main;
+        spellParticles.startColor = color;
+    }
+}
